304d5c25e0f12a2b4168882b4f38332e68f9a795
Felix Beer <mail@felix.beer>
2023-11-22 16:33:11 +0100

    Fix helper for getting listing item

--- resources/views/blocks/templates/floating-header-blog.blade.php
+++ resources/views/blocks/templates/floating-header-blog.blade.php
@@ -4,7 +4,7 @@
     <img class="absolute h-full w-full object-cover"
          src="{{get_single_media($backgroundImage) ?: 'https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80'}}"
          alt="header-image">
-    <div class="absolute h-full w-full bg-gradient-to-tl from-primary-800/50 to-primary-300/50"></div>
+    <div class="absolute h-full w-full bg-gradient-to-tl from-primary-800/70 to-primary-300/60"></div>
     <div class="container mx-auto px-4 h-full">
         <div class="relative sm:overflow-hidden h-full flex flex-col sm:flex-row items-center justify-center sm:pt-0">
             <div class="relative flex items-center justify-center flex-col pl-4">
@@ -12,10 +12,14 @@
                     @php
                         $item = get_listing_item($category);
                     @endphp
-                    @svg($item->icon, 'w-4 h-4 mr-1')
-                    {{
-                        $item->name
-                    }}
+                    @if($item)
+                        @if($item->icon)
+                            @svg($item->icon, 'w-4 h-4 mr-1')
+                        @endif
+                        {{
+                            $item->name
+                        }}
+                    @endif
                 </span>
                 <h1 class="text-4xl text-left font-extrabold drop-shadow tracking-tight sm:text-5xl lg:text-7xl underline decoration-secondary-500/50">
                     <span class="block text-secondary-400 drop-shadow-xl">{{$title ?: 'Lorem ipsum'}}</span>
