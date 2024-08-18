26b1ef5d58e96f27c162b3d369d9458b027d460e
Felix Beer <mail@felix.beer>
2024-08-18 15:12:07 +0200

    Fully remove, not just use HTML comments 🤦

--- resources/views/member-area/post/show.blade.php
+++ resources/views/member-area/post/show.blade.php
@@ -1,3 +1,2 @@
 <x-daugt::layouts.member-area-base>
-<!--<x-daugt::blocks-renderer :data="$post->blocks"></x-daugt::blocks-renderer>-->
 </x-daugt::layouts.member-area-base>
--- resources/views/shop/product/show.blade.php
+++ resources/views/shop/product/show.blade.php
@@ -1,3 +1,2 @@
 <x-daugt::layouts.base :title="$product->name">
-    <!--<x-daugt::blocks-renderer :data="$product->description"></x-daugt::blocks-renderer>-->
 </x-daugt::layouts.base>
