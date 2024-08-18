de72b94faa639ef5a4db5c0c874080d86eaca558
Felix Beer <mail@felix.beer>
2024-08-18 15:10:26 +0200

    Remove old blocks-renderer components in templates

--- resources/views/member-area/post/show.blade.php
+++ resources/views/member-area/post/show.blade.php
@@ -1,3 +1,3 @@
 <x-daugt::layouts.member-area-base>
-<x-daugt::blocks-renderer :data="$post->blocks"></x-daugt::blocks-renderer>
+<!--<x-daugt::blocks-renderer :data="$post->blocks"></x-daugt::blocks-renderer>-->
 </x-daugt::layouts.member-area-base>
--- resources/views/shop/product/show.blade.php
+++ resources/views/shop/product/show.blade.php
@@ -1,3 +1,3 @@
 <x-daugt::layouts.base :title="$product->name">
-    <x-daugt::blocks-renderer :data="$product->description"></x-daugt::blocks-renderer>
+    <!--<x-daugt::blocks-renderer :data="$product->description"></x-daugt::blocks-renderer>-->
 </x-daugt::layouts.base>
