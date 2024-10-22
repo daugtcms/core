af75a96e043e01d787115721b1ee57eee073ea67
Felix Beer <mail@felix.beer>
2024-10-23 00:22:54 +0200

    fix center-auth media fetching

--- resources/views/blocks/templates/center-auth.blade.php
+++ resources/views/blocks/templates/center-auth.blade.php
@@ -1,14 +1,16 @@
 <div class="min-h-screen w-full relative flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-neutral-100">
 
     @if($background)
-    <div class="absolute inset-0 bg-center bg-cover bg-no-repeat" style="background-image: url('{{ get_single_media($background) }}')">
+    <div class="absolute inset-0 bg-center bg-cover bg-no-repeat" style="background-image: url('{{$background['url']}}')">
         <div class="absolute inset-0 bg-black/30 backdrop-blur"></div>
     </div>
     @endif
 
+    @if($logo)
     <div class="z-10">
-        <img src="{{ get_single_media($logo) }}" class="max-w-xs max-h-20 drop-shadow-xl">
+        <img src="{{ $logo['url'] }}" class="max-w-xs max-h-20 drop-shadow-xl">
     </div>
+    @endif
 
     <div class="z-10 w-full sm:max-w-md mt-6 px-5 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
