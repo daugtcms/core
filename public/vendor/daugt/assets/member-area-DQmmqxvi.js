1d6fcc54392c5342a1a5f0a6177c53afba8a15f2
Felix Beer <mail@felix.beer>
2024-10-23 23:34:12 +0200

    back to bunny

---
+++ public/vendor/daugt/assets/member-area-DQmmqxvi.js
@@ -0,0 +1,1 @@
+import{j as l,k as e,l as i,L as u}from"./tiptap-C5sHkbfq.js";import{P as a,m as p,a as d}from"./unocss-2XZZCeQ9.js";window.Plyr=a;Alpine.plugin(l);Alpine.plugin(e);Alpine.plugin(p);Alpine.plugin(d);Alpine.plugin(i);u.start();
