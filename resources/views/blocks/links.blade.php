b97c2705cb319ad68ffb864cae347344affc24d7
Felix Beer <mail@felix.beer>
2024-10-25 23:58:33 +0200

    move background image

--- resources/views/blocks/links.blade.php
+++ resources/views/blocks/links.blade.php
@@ -1,12 +1,13 @@
-<div class="rounded-md container relative bg-cover bg-center mx-4 px-0" @isset($backgroundImage)style="background-image: url('{{($backgroundImage['url'])}}')"@endisset>
+<div class="rounded-md container relative">
     @isset($backgroundImage)
+        <div class="absolute inset-0 bg-cover bg-center" style="background-image: url('{{($backgroundImage['url'])}}')"></div>
         <div class="absolute inset-0 bg-primary-500/35 pointer-events-none"></div>
     @endisset
-    <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8 z-10">
+    <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
         @isset($text)
         <h2
             @class([
-            'text-3xl font-extrabold sm:text-4xl mb-4 font-accent drop-shadow-lg',
+            'text-3xl font-extrabold sm:text-4xl mb-4 font-accent drop-shadow-lg z-10',
             'text-white' => isset($backgroundImage),
             'text-primary-600' => !isset($backgroundImage),
             ])>
@@ -14,7 +15,7 @@
         </h2>
         @endisset
 
-        <div class="flex justify-center gap-x-4 flex-wrap">
+        <div class="flex justify-center gap-x-4 flex-wrap z-10">
 
             @if($link1)
                 <x-daugt::form.button style="primary" :href="$link1['url']">{{$link1['text']}}</x-daugt::form.button>
